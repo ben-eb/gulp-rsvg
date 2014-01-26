3e307f18fdef5f7db1b4654b1d9c71c728440963
Ben Briggs <therealbenbriggs@hotmail.com>
2014-01-26 23:42:13 +0000

    Adds streaming support.

--- index.js
+++ index.js
@@ -2,25 +2,67 @@
 
 'use strict';
 
-var gutil = require('gulp-util'),
-    Rsvg  = require('rsvg').Rsvg,
-    map   = require('map-stream');
+var Rsvg          = require('rsvg').Rsvg,
+    gutil         = require('gulp-util'),
+    transform     = require('stream').Transform,
+    bufferstreams = require('bufferstreams'),
 
-module.exports = function() {
-    var format = 'png';
+    PLUGIN_NAME   = 'gulp-rsvg';
+
+function rsvgTransform(options) {
+    // Returns a callback that handles the buffered content
+    return function(err, buffer, cb) {
+        if (err) {
+            cb(gutil.PluginError(PLUGIN_NAME, err));
+        }
+        var svg = new Rsvg(String(buffer));
+        var transform = svg.render({
+            format: options.format,
+            width: options.width || svg.width,
+            height: options.height || svg.height
+        }).data;
+        cb(null, new Buffer(transform));
+    };
+}
+
+function gulprsvg() {
+    var options = {
+        format: 'png'
+    };
     if (arguments.length > 0) {
-        format = arguments[0].format || format;
-        var width = arguments[0].width || false;
-        var height = arguments[0].height || false;
+        options.format = arguments[0].format || format;
+        options.width = arguments[0].width || false;
+        options.height = arguments[0].height || false;
     }
-    return map(function(file, cb) {
-        var svg = new Rsvg(file.contents);
-        file.path = gutil.replaceExtension(file.path, '.' + format);
-        file.contents = new Buffer(svg.render({
-            format: format,
-            width: width || svg.width,
-            height: height || svg.height
-        }).data);
-        cb(null, file);
-    });
-};
+    var stream = new transform({ objectMode: true });
+
+    stream._transform = function(file, unused, done) {
+        // Pass through if null
+        if (file.isNull()) {
+            stream.push(file);
+            done();
+            return;
+        }
+
+        if (file.isStream()) {
+            file.contents = file.contents.pipe(new bufferstreams(rsvgTransform(options)));
+            stream.push(file);
+            done();
+        } else {
+            var svg = new Rsvg(file.contents);
+            file.path = gutil.replaceExtension(file.path, '.' + options.format);
+            file.contents = new Buffer(svg.render({
+                format: options.format,
+                width: options.width || svg.width,
+                height: options.height || svg.height
+            }).data);
+            stream.push(file);
+            done();
+        }
+    };
+
+    return stream;
+}
+
+gulprsvg.rsvgTransform = rsvgTransform;
+module.exports = gulprsvg;
--- test.js
+++ test.js
@@ -5,6 +5,8 @@
 
 var convert = require('./index'),
     expect  = require('chai').expect,
+    Stream  = require('stream'),
+    es      = require('event-stream'),
     gutil   = require('gulp-util');
 
 var raw = '<svg xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="40" fill="#ff00"/></svg>';
@@ -40,4 +42,40 @@ describe('gulp-rsvg', function() {
             path: 'test.svg'
         }));
     });
+    it('should work the same in stream mode', function(cb) {
+        var stream = convert();
+        var fakeFile = new gutil.File({
+            contents: new Stream()
+        });
+
+        stream.on('data', function(data) {
+            data.contents.pipe(es.wait(function(err, data) {
+                expect(data).to.not.equal(raw);
+                cb();
+            }));
+        });
+
+        stream.write(fakeFile);
+        fakeFile.contents.write(raw);
+        fakeFile.contents.end();
+    });
+    it('should let null files pass through', function(cb) {
+        var stream = convert(),
+            n = 0;
+
+        stream.pipe(es.through(function(file) {
+            expect(file.path).to.equal('null.md');
+            expect(file.contents).to.equal(null);
+            n++;
+        }, function() {
+            expect(n).to.equal(1);
+            cb();
+        }));
+
+        stream.write(new gutil.File({
+            path: 'null.md',
+            contents: null
+        }));
+        stream.end();
+  });
 });
